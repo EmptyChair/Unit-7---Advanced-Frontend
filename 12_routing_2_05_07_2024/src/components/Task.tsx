1dfef589c5d8614b65c41ddc2bb1f13a5611a894
EmptyChair <romaswebex01@gmail.com>
2024-07-08 23:22:00 +0200

    l12

---
+++ 12_routing_2_05_07_2024/src/components/Task.tsx
@@ -0,0 +1,95 @@
+import { FunctionComponent, RefObject, useRef, useState } from "react";
+import { ITask } from "./TaskList";
+import "bootstrap/dist/css/bootstrap.min.css";
+/////////////////////
+//TASK
+/////////////////////
+
+interface IProps {
+  task: ITask,
+  deleteTask: () => void,
+  editTask: (i: number, updatedTask: ITask) => void,
+  index: number
+}
+
+//also possible to write FC
+const Task: FunctionComponent<IProps> = ({
+  task: { number, title, isComplete, updatedAt },
+  deleteTask,
+  editTask,
+  index,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const textRef: RefObject<HTMLTextAreaElement> = useRef(null); //???
+
+  const handleClickSave = () => {
+    if (textRef.current) {
+      editTask(index, {
+        number,
+        title: textRef.current.value,
+        isComplete,
+        updatedAt: new Date().toISOString(),
+      });
+      setIsEdit(false);
+    }
+  };
+
+  return (
+    <div
+      className={`card mb-3 ${isComplete ? "bg-light" : ""}`}
+      style={{ boxShadow: "0 4px 8px rgba(0,0,0,0.1)" }}
+    >
+      <div className="card-body">
+        {isEdit ? (
+          <div>
+            <textarea
+              ref={textRef}
+              defaultValue={title}
+              className="form-control mb-2"
+            ></textarea>
+            <button
+              onClick={handleClickSave}
+              className="btn btn-success btn-sm me-2"
+            >
+              Save
+            </button>
+          </div>
+        ) : (
+          <div className="d-flex align-items-center">
+            <div>
+              {number}.&nbsp;
+            </div>
+            <p
+              className={`mb-0 ${isComplete ? "text-decoration-line-through text-muted" : ""
+                }`}
+              style={{ flexGrow: 1 }}
+            >
+              {title}
+            </p>
+            <small className="text-muted me-5">Updated on: {new Date(updatedAt).toLocaleString()}</small>
+            <input
+              checked={isComplete}
+              onChange={() =>
+                editTask(index, { title, isComplete: !isComplete, updatedAt: new Date().toISOString() })
+              }
+              type="checkbox"
+              className="form-check-input me-2"
+            />
+            <button
+              onClick={() => setIsEdit(true)}
+              className="btn btn-warning btn-sm me-2"
+            >
+              Edit
+            </button>
+            <button onClick={deleteTask} className="btn btn-danger btn-sm">
+              Del
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
+export default Task;
---
+++ 12_routing_2_05_07_2024/src/components/UserListCC.tsx
@@ -0,0 +1,228 @@
+//////////////////////////
+// USERS LIST
+// USER: name, job, place - take from JASOn placeholder
+/////////////////////77
+
+import { ChangeEvent, Component, ReactNode } from "react";
+import axios from "axios";
+import UserCC from "./UserCC";
+
+export interface IUser {
+  id: number;
+  name: string;
+  company: { name: string };
+  phone: string;
+}
+
+interface IState {
+  users: IUser[];
+  //newUser: IUser without an id
+  newUser: Omit<IUser, "id">;
+  //newName: string,
+  //newCompany: string;
+  //newPhone: string
+}
+
+class UserListCC extends Component<Record<string, never>, IState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      users: [],
+      newUser: {
+        company: {
+          name: "",
+        },
+        name: "",
+        phone: "",
+      },
+      //newCompany:"",
+      //newName:"",
+      //newPhone:"",
+    };
+  }
+
+  //before rendering (when we load the page) we must make request and then save state
+  componentDidMount(): void {
+    axios
+      .get<IUser[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        this.setState({ ...this.state, users: this.sortByName(response.data) });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  sortByName = (array: IUser[]) => {
+    return array.sort((a, b) => a.name.localeCompare(b.name));
+  };
+
+  // each class will have this function/property
+  addUser = () => {
+    this.setState((prevState) => ({
+      //store new values as a new user and add it to the existing array of users
+      users: [
+        {
+          id: this.state.users.length + 1,
+          company: { name: this.state.newUser.company.name },
+          name: this.state.newUser.name,
+          phone: this.state.newUser.phone,
+        },
+        ...prevState.users,
+      ],
+      //wipe new values from the input window
+      newUser: {
+        company: {
+          name: "",
+        },
+        name: "",
+        phone: "",
+      },
+    })),
+    this.setState((prevState) => ({
+      users: this.sortByName(prevState.users),
+    }));
+  };
+
+  // ADD USER with destructured version
+  /*
+    addUser2 = () => {
+        const { length } = this.state.users;
+        const { company : {name: companyName}, name, phone } = this.state.newUser;
+
+        this.setState((prevState) => ({
+            //store new values as a new user and add it to the existing array of users
+            users: [
+                {
+                    id: length + 1,
+                    company: { name: companyName },
+                    name,
+                    phone,
+                },
+                ...prevState.users,
+            ],
+            //wipe new values from the input window
+            newUser: {
+                company: {
+                    name: "",
+                },
+                name: "",
+                phone: "",
+            },
+        }));
+    };
+    */
+
+  deleteUser = (userId: number) => {
+    this.setState((prevState) => ({
+      users: [...prevState.users.filter((user) => user.id !== userId)],
+      newUser: { ...prevState.newUser },
+    }));
+  };
+
+  //extra index not needed! In this case each user has a unique id, so we don't need to provide index to this function
+  editUser = (updatedUser: IUser) => {
+    this.setState((prevState) => ({
+      users: [
+        ...prevState.users.map((e) =>
+          e.id === updatedUser.id ? updatedUser : e
+        ),
+      ],
+      newUser: { ...prevState.newUser },
+    }));
+    this.setState((prevState) => ({
+      users: this.sortByName(prevState.users),
+    }));
+  };
+
+  //hand over element and key
+
+  inputData = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    if (name === "companyName") {
+      this.setState({
+        ...this.state,
+        newUser: { ...this.state.newUser, company: { name: value } },
+      });
+      return;
+    }
+    this.setState({
+      ...this.state,
+      newUser: {
+        ...this.state.newUser,
+        [name === "name" ? "name" : "phone"]: event.target.value,
+      },
+    });
+  };
+
+  render(): ReactNode {
+    return (
+      <div className="container mt-4">
+        <h1 className="mb-4 text-center text-light bg-dark">User List App</h1>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            value={this.state.newUser.name}
+            name="name"
+            //target - what changed onCHange
+            /*
+            onChange={(e) => 
+                (
+                this.setState({ ...this.state, newUser: { ...this.state.newUser, name: e.target.value } })
+                )
+            }
+            */
+            onChange={(e) => this.inputData(e)}
+            className="form-control"
+            placeholder="New name"
+          />
+
+          <input
+            type="text"
+            value={this.state.newUser.company.name}
+            name="companyName"
+            onChange={(e) => this.inputData(e)}
+            className="form-control"
+            placeholder="New company name"
+          />
+
+          <input
+            type="text"
+            value={this.state.newUser.phone}
+            name="phone"
+            onChange={(e) => this.inputData(e)}
+            className="form-control"
+            placeholder="New phone number"
+          />
+
+          <button onClick={this.addUser} className="btn btn-primary">
+            Add User
+          </button>
+        </div>
+
+        <div>
+          {this.state.users.map((user) => (
+            <UserCC
+              key={user.id}
+              user={user}
+              //wrapper function which tells that this function is to be started with this parameter
+              deleteUser={() => this.deleteUser(user.id)}
+              editUser={this.editUser}
+            />
+          ))}
+        </div>
+        <div className="fixed-bottom mx-3 d-flex justify-content-end ">
+        <p className="fw-bold border border-dark px-3 py-2">
+          <div className="d-flex justify-content-end">
+            Author: Roman Sheludko
+          </div>
+          <div className="d-flex justify-content-end">Group FS-40-2</div>
+          <div className="d-flex justify-content-end">July 2024</div>
+        </p>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default UserListCC;
